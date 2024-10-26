4d64eda42ae3d2c486f0dee9638d44c7b89e1288
Jean Schramm <gigantsc@gmail.com>
2024-10-25 21:48:12 -0300

    volta correção dataframe

--- dashboard.py
+++ dashboard.py
@@ -389,7 +389,24 @@ def painel_mensagem():
     today = datetime.today()
 
     #customcode inicio
+      # Verificar se o DataFrame está vazio
+    if df.empty:
+        st.warning("Sem dados disponíveis no DataFrame.")
+        st.stop()  # Interrompe a execução se não houver dados
 
+    # Verificar as colunas disponíveis no DataFrame
+    st.write("Colunas disponíveis no DataFrame:", df.columns.tolist())
+
+    # Remover espaços extras nos nomes das colunas e converter tudo para minúsculas para facilitar a comparação
+    df.columns = df.columns.str.strip().str.lower()
+
+    # Verificar se a coluna 'data de criação' (em minúsculas) existe
+    if 'data de criação' not in df.columns:
+        st.error("Coluna 'Data de Criação' não encontrada no DataFrame. As colunas disponíveis são: " + ", ".join(df.columns))
+        st.stop()
+
+    # Ajustar para o nome correto da coluna em minúsculas
+    temp_dates = pd.to_datetime(df['data de criação'], format='%d/%m/%y %H:%M:%S', dayfirst=True, errors='coerce')
  
     #customcode fim
 
